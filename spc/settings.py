f2a8fda4eca05bef39239b35ac435049a813a302
Yash Jain <yash.jain3599@gmail.com>
2018-10-17 18:02:09 +0530

    Hopefully REST API is now working

--- spc/settings.py
+++ spc/settings.py
@@ -73,6 +73,14 @@ TEMPLATES = [
         },
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
 
 WSGI_APPLICATION = 'spc.wsgi.application'
 
