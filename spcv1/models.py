948436ae6e83cd617bd246bdc00b48300fc1f97f
Yash Jain <yash.jain3599@gmail.com>
2018-10-17 21:42:26 +0530

    Changed database schema

--- spcv1/models.py
+++ spcv1/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import Permission, User
 
 class File(models.Model):
-    # user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
     path = models.CharField(max_length=1000)
-    data = models.FileField(max_length=100)
+    data = models.BinaryField(editable=True)
     timestamp = models.FloatField()
     def __str__(self):
         return self.path
--- spcv1/views.py
+++ spcv1/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+#import MySQLdb, cPickle
 def home(request):
     return render(request, 'home.html')
 
@@ -28,6 +29,38 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
+
+# def insertBLOB():
+#     # Connect to a DB, e.g., the test DB on your localhost, and get a cursor
+#     connection = MySQLdb.connect(db="db.sqlite3")
+#     cursor = connection.cursor(  )
+
+#     # Make a new table for experimentation
+#     cursor.execute("CREATE TABLE justatest (name TEXT, ablob BLOB)")
+
+#     try:
+#         # Prepare some BLOBs to insert in the table
+#         names = 'aramis', 'athos', 'porthos'
+#         data = {}
+#         for name in names:
+#             datum = list(name)
+#             datum.sort(  )
+#             data[name] = cPickle.dumps(datum, 1)
+
+#         # Perform the insertions
+#         sql = "INSERT INTO justatest VALUES(%s, %s)"
+#         for name in names:
+#             cursor.execute(sql, (name, MySQLdb.escape_string(data[name])) )
+
+#         # Recover the data so you can check back
+#         sql = "SELECT name, ablob FROM justatest ORDER BY name"
+#         cursor.execute(sql)
+#         for name, blob in cursor.fetchall(  ):
+#             print name, cPickle.loads(blob), cPickle.loads(data[name])
+#     finally:
+#         # Done. Remove the table and close the connection.
+#         cursor.execute("DROP TABLE justatest")
+#         connection.close(  )
 
 #List all users, with their file paths and time-stamp
 #user/filename
